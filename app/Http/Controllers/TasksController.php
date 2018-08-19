81b2605a7ec2fce7fa17cdfc715aa16d93d92a9e
jiro.kirameki <ec2-user@ip-172-31-93-35.ec2.internal>
2018-08-19 22:29:11 +0000

    varidation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -43,8 +43,13 @@ class TasksController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
+    {   
+        $this->validate($request, [
+            'status' => 'required|max:10',
+        ]);
+        
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -91,6 +96,7 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $task = Task::find($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,7 +5,10 @@
 <h1>メッセージ新規作成ページ</h1>
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
-
+        
+         {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}
+        
         {!! Form::label('content', 'メッセージ:') !!}
         {!! Form::text('content') !!}
 
